f7e9d4a031313ace59acf7fc4830cbcf314c04e5
Marcelo Soares <ceeloalves@hotmail.com>
2024-04-09 15:27:19 -0300

    signals no django (trigers)
    -Usando signals na pratica
    -Criando nosso inventario
    -Atualizando o inventario com signals

---
+++ cars/signals.py
@@ -0,0 +1,31 @@
+from django.db.models.signals import pre_save, pre_delete, post_delete, post_save
+from django.dispatch import receiver
+from django.db.models import Sum
+from cars.models import Car, CarInventory
+
+def car_inventory_update():
+    total_cars_count = Car.objects.all().count()
+    sum_cars_value = Car.objects.aggregate(total_value=Sum('value'))['total_value']
+    CarInventory.objects.create(cars_count=total_cars_count, cars_value= sum_cars_value)
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    car_inventory_update()
+
+
+@receiver(pre_delete, sender=Car)
+def car_pre_delte(sender, instance, **kwargs):   
+    car_inventory_update()
+
+
+'''
+@receiver(post_save, sender=Car)
+def car_post_save(sender, instance, **kwargs):
+    #print(instance.model, instance.brand)
+
+
+@receiver(post_delete, sender=Car)
+def car_post_delete(sender, instance, **kwargs):   
+    pass
+'''
