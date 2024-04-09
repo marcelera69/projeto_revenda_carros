32ddb548b335df5d7ab5159bcb4dca780234ccd4
Marcelo Soares <ceeloalves@hotmail.com>
2024-04-09 10:03:31 -0300

    criando o repositório

---
+++ cars/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from cars.models import Car
+import datetime
+
+'''
+class CarForm(forms.Form):
+    model = forms.CharField(max_length=200)
+    brand = forms.ModelChoiceField(Brand.objects.all())
+    factory_year= forms.IntegerField()
+    model_year= forms.IntegerField()
+    plate = forms.CharField(max_length=10)
+    value= forms.FloatField()
+    photo = forms.ImageField()
+'''
+
+
+class CarForm(forms.ModelForm):
+
+    class Meta():
+        model = Car
+        fields = '__all__'
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+
+        if value < 0:
+            self.add_error('value', 'O valor do Automóvel tem que ser maior que R$0')
+        
+        return value
+    
+
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+
+        if factory_year == None:
+            return factory_year
+
+        if factory_year > datetime.datetime.now().year:
+            self.add_error('factory_year', f'O ano de fabricação não pode ser maior que {datetime.datetime.now().year}')
+
+        return factory_year
+    
+
+    def clean_model_year(self):
+        model_year = self.cleaned_data.get('model_year')
+
+        if model_year < 1975:
+            self.add_error('model_year', 'O modelo do carro é muito antigo para a nossa loja (acima de 1975)')
+
+        return model_year
+    
---
+++ cars/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
+    factory_year= models.IntegerField(blank=True, null=True)
+    model_year= models.IntegerField()
+    plate = models.CharField(max_length=10, blank=True, null=True)
+    value= models.FloatField()
+    photo = models.ImageField(upload_to='cars/')
+
+
+    def __str__(self):
+        return self.model
