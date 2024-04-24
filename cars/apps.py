a1271e8e22238a53da2d03fdcde931b12b3341b8
Marcelo Soares <ceeloalves@hotmail.com>
2024-04-24 11:22:16 -0300

    . teste

--- cars/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CarsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cars'
-
-    def ready(self):
-        import cars.signals
